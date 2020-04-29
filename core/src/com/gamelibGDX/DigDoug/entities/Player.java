60d412deac9299f6981661666d5ddce945198f1c
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2020-04-28 23:05:50 -0500

    Bruh

--- core/src/com/gamelibGDX/DigDoug/entities/Player.java
+++ core/src/com/gamelibGDX/DigDoug/entities/Player.java
@@ -5,18 +5,25 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.gamelibGDX.DigDoug.tools.CollisionRect;
 import com.gamelibGDX.DigDoug.world.GameMap;
 
 public class Player extends Entity {
 	
 	private static final int SPEED = 80;
 	private static final int JUMP_VELOCITY = 5;
-	
+	public static final int WIDTH = 14;
+	public static final int HEIGHT = 32;
+
+	CollisionRect rect;
 	Texture image;
 
 	public Player (float x, float y, GameMap map) {
+
 		super(x, y, EntityType.PLAYER, map);
 		image = new Texture("C:\\Users\\Logan\\Documents\\GitHub\\DigDoug\\core\\assets\\player.png");
+
+		this.rect = new CollisionRect(x, y, WIDTH, HEIGHT);
 	}
 	
 	@Override
