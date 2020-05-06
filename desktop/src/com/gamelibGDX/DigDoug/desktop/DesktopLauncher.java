b49da3f3ae45ef552559079048b6741f0ff28d5a
cole-giles <54542504+cole-giles@users.noreply.github.com>
2020-05-06 15:52:14 -0500

    bruh

--- desktop/src/com/gamelibGDX/DigDoug/desktop/DesktopLauncher.java
+++ desktop/src/com/gamelibGDX/DigDoug/desktop/DesktopLauncher.java
@@ -4,27 +4,28 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.ApplicationListener;
 import com.gamelibGDX.DigDoug.DigDoug;
-import com.badlogic.gdx.Game;
 
 import javax.swing.*;
 
 public class DesktopLauncher {
 	public static void main (String[] args) {
+		ImageIcon logo = new ImageIcon("logo.png");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
 		int selection;
-		boolean isYes;
-		boolean isNo;
+		Object[] options = {"Play", "Leaderboard","Quit"};
 
-		selection = JOptionPane.showConfirmDialog(null, "Would you like to play DigDug?");
-		isYes = (selection == JOptionPane.YES_OPTION);
-		isNo = (selection == JOptionPane.NO_OPTION);
-		if(isYes == true){
+
+		selection = JOptionPane.showOptionDialog(null, "Would you like to play DigDug?", "Dig Doug", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, logo, options, options[2]);
+		if(selection == JOptionPane.YES_OPTION){
 			new LwjglApplication((ApplicationListener) new DigDoug(), config);
 		}
-		else if(isNo){
-			JOptionPane.showConfirmDialog(null, "Coward");
+		else if(selection != JOptionPane.YES_OPTION && selection != JOptionPane.NO_OPTION){
+			JOptionPane.showMessageDialog(null, "Coward");
+		}
+		else if(selection == JOptionPane.NO_OPTION){
+			JOptionPane.showMessageDialog(null, "In Progress, leave me alone.");
 		}
 	}
 }
